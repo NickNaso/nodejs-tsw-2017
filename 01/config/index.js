3d41c571881cf8e718f0c6bc10cf245bdce4e5cc
NickNaso <nicoladelgobbo@gmail.com>
2017-12-18 19:53:31 +0100

    Fixed typo on challenge 01

--- 01/config/index.js
+++ 01/config/index.js
@@ -4,7 +4,7 @@ const config = require('./config.json')
 
 /**
  * Identify the right environment first based on system environment varialble
- * process.NODE_ENV then on value present on config and export the configuration
+ * process.env.NODE_ENV then on value present on config and export the configuration
  * for all the application
  */
 
