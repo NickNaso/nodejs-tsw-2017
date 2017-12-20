80e0a426ed5a9bec6ee492323c31229843bde2d2
NickNaso <nicoladelgobbo@gmail.com>
2017-12-20 11:51:09 +0100

    Removed unuseful resources

--- final/config/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const config = require('./config.json')
-
-const env = process.env.NODE_ENV || config.env
-
-module.exports = config[env]
--- final/lib/passport/index.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-
-exports.LocalStrategy = require('./LocalStrategy')
